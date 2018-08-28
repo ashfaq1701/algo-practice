ea21ad0dc388a528287cb0e5f314dea5346b4ec4
Md Ashfaq Salehin <ashfaq.aws@gmail.com>
2018-08-29 01:38:57 +0700

    Create codility-4-2.java

---
+++ codility/codility-4-2.java
@@ -0,0 +1,19 @@
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int X, int[] A) {
+        // write your code in Java SE 8
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < A.length; i++) {
+            set.add(A[i]);
+            if (set.size() == X) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
