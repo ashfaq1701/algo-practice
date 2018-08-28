56dc03efdedfc7ecb9a327241947ad58a7e89ad8
Md Ashfaq Salehin <ashfaq.aws@gmail.com>
2018-08-29 02:50:50 +0700

    Update codility-4-3.java

--- codility/codility-4-3.java
+++ codility/codility-4-3.java
@@ -8,19 +8,22 @@ class Solution {
     public int[] solution(int N, int[] A) {
         // write your code in Java SE 8
         int max = 0;
+        int lastMax = 0;
         int[] states = new int[N];
         for (int i = 0; i < A.length; i++) {
             if (A[i] == N + 1) {
-                for (int j = 0; j < states.length; j++) {
-                    states[j] = max;
-                }
+                lastMax = max;
             } else {
+                states[A[i] - 1] = Math.max(states[A[i] - 1], lastMax);
                 states[A[i] - 1] = states[A[i] - 1] + 1;
                 if (states[A[i] - 1] > max) {
                     max = states[A[i] - 1];                    
                 }
             }
         }
+        for(int i = 0 ; i < states.length; i++) {
+			states[i] = Math.max(states[i], lastMax);
+		}
         return states;
     }
 }
