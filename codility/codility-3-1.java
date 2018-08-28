44f87411250493728f115f89d886c273aed627d1
Md Ashfaq Salehin <ashfaq.aws@gmail.com>
2018-08-28 21:52:47 +0700

    Create codility-3-1.java

---
+++ codility/codility-3-1.java
@@ -0,0 +1,13 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        double result = (double) (Y-X) / (double) D;
+        return (int) Math.ceil(result);
+    }
+}
